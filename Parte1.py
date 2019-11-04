aee478db105b22e364ebbcd14bcbd180490f1e47
Cecily Chaquilla <cecilychaquilla@gmail.com>
2019-11-04 10:18:17 -0500

    PARTE 1: ACT 7

--- Parte1.py
+++ Parte1.py
@@ -1,27 +1,27 @@
 import os
 os.system("cls")
 
-#Cportland = [[I],[II],[V], [IP], [HS], [GU]]
-#I = ["quisqueya = s/ 21.50","sol = s/ 22.20","pacasmayo = s/ 24.00"]
-#II = ["Andino = s/ 24.00"]
-#V = ["Pacasmayo = s/ 31.90"]
-#IP = ["Quisqueya = s/ 20.00", "Yura = s/ 20.60", "Andino = s/ 23.20"]
-#HS = ["Inka = s/ 22.80", "Andino = s/ 24.20"]
-#GU = ["APU = s/ 20.50", "Pacasmayo = s/ 23.10"]
+I = ["Quisqueya = s/ 21.50","Sol = s/ 22.20","Pacasmayo = s/ 24.00"]
+II = ["Andino = s/ 24.00"]
+V = ["Pacasmayo = s/ 31.90"]
+IP = ["Quisqueya = s/ 20.00", "Yura = s/ 20.60", "Andino = s/ 23.20"]
+HS = ["Inka = s/ 22.80", "Andino = s/ 24.20"]
+GU = ["APU = s/ 20.50", "Pacasmayo = s/ 23.10"]
+Cportland = [[I], [II], [V], [IP], [HS], [GU]]
 
-#arena fina = ["arena (Unicon) = s/ 0.16xkg], "arena Hades = s/ 1.43xkg]
+arena_fina = ["arena (Unicon) = s/ 0.16xkg", "arena Hades = s/ 1.43xkg"]
 
-#piedra =["piedra chancada = s/ 0.15 solesxkg]
+piedra =["piedra chancada = s/ 0.15 solesxkg"]
 
-#acero = [[barra_corrugada_1/2], [barra_corrugada_3/8],[barra_corrugada_8mm]]
-#barra_corrugada_1/2 = ["Aceros Arequipa = s/ 27.70", SiderPeru = s/ 27.00]
-#barra_corrugada_3/8 = ["Aceros Arequipa = s/ 15.50", "SiderPeru = s/ 15.50"]
-#barra_corrugada_8mm = ["Aceros Arequipa = s/ 11.20", SiderPeru = s/ 11.20]
+barra_corrugada_1_2 = ["Aceros Arequipa = s/ 27.70", "SiderPeru = s/ 27.00"]
+barra_corrugada_3_8 = ["Aceros Arequipa = s/ 15.50", "SiderPeru = s/ 15.50"]
+barra_corrugada_8mm = ["Aceros Arequipa = s/ 11.20", "SiderPeru = s/ 11.20"]
+acero = [[barra_corrugada_1_2], [barra_corrugada_3_8],[barra_corrugada_8mm]]
 
-#madera = [[madera_viga]]
-#madera_viga = [["Maestro_2x3x16 = s/ 46.90"], ["Sodimac_2x4x16 = s/ 57.90"]]
+madera_viga = ["Maestro_2x3x16 = s/ 46.90", "Sodimac_2x4x16 = s/ 57.90"]
+madera = [[madera_viga]]
 
-#listaP = [[Cportland], arena, piedra, [acero], [madera]]
+listaP = [[Cportland], [arena_fina], [piedra], [acero], [madera]]
 
 precio = []
 while True:
@@ -35,7 +35,9 @@ for i in range(cantidad):
     nombre = int(input("Ingrese el número correspondiente al producto" + num + ":"))
     metrado = input("Ingrese el metrado del producto " + num + ":")
     pos = 1 - nombre
+    print(listaP[pos], "Escoja la opción del producto:")
+    if listaP[pos] == listaP[0]:
+        print("Cemento Portland tipo I: 1","Cemento Portland tipo II: 2","Cemento Portland tipo V: 3",
+        "Cemento Portland tipo IP: 4","Cemento Portland tipo HS: 5","Cemento Portland tipo GU: 6")
 
-
-    
 
