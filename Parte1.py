7641679014ecb7fc4b2c054d146dadf30afb863b
Mirella Rubio <mirellarubiosailorm@gmail.com>
2019-11-04 09:00:37 -0500

    parte 1: actualizacion 2

--- Parte1.py
+++ Parte1.py
@@ -1,19 +1,23 @@
 import os
 os.system("cls")
 
+Cportland = [I,II,V, IP, HS]
+listaP = [[Cportland], arena, piedra, acero, madera]
+
+
+precio = []
 while True:
     cantidad = int(input("¿Cuántos productos va utilizar?"))
     if 0 < cantidad < 6:
         break 
 
-lista = []
-
-precio = []
-
 for i in range(cantidad):
     num = str (i + 1)
+    print("Escoja una opcion")
+    print ("Cemento: 1")
     nombre = input("Ingrese el nombre del producto " + num + ":")
     metrado = input("Ingrese el metrado del producto " + num + ":")
 
-    producto =[nombre, metrado]
-    lista.append(producto)
+
+    
+
